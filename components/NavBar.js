05a486acd25c54316f7bcf456fad5695c1274bd3
liqiang372 <liqiang372@gmail.com>
2021-01-25 21:58:03 -0800

    fix root path

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,7 +9,7 @@ export function Navbar({ fixed }) {
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 px-4 whitespace-no-wrap uppercase text-white bg-purple-1000 rounded-3xl"
-              href="#pablo"
+              href="/"
             >
               EASY REACTIVE
             </a>
